e0afb9fa593665f54857adb3f23f868c0c98a649
dhanyg <ramayoga04@gmail.com>
2020-08-14 18:56:37 +0700

    membuat file migration

---
+++ database/migrations/2020_08_14_114946_create_vote_jawaban_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVoteJawabanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vote_jawaban', function (Blueprint $table) {
+            $table->id();
+            $table->string('vote');
+            $table->unsignedBigInteger('jawaban_id')->nullable();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('jawaban_id')->references('id')->on('jawaban');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vote_jawaban');
+    }
+}
